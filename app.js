68d2799e71bc2cd745ec6b38029b0b5aa53e7c9b
manaswebrepo <mymanas@gmail.com>
2020-03-31 17:15:36 +0530

    user model and connection

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const Event = require("./models/event");
+const User = require("./models/user");
 
 const app = express();
 
@@ -21,18 +23,34 @@ app.use(
       price: Float!
       date: String!
     }
+
+    type User {
+      _id: ID
+      email: String!
+      password: String
+    }
+    
     input EventInput {
       title: String!
       description: String!
       price: Float!
       date: String!
     }
+
+    input UserInput{
+      email: String!
+      password: String!
+    }
+
     type RootQuery { 
         events: [Event!]!
     }
+
     type RootMutation { 
         createEvent(inputEvent: EventInput): Event
+        createUser(inputUser: UserInput): User
     }
+
     schema {
         query : RootQuery
         mutation: RootMutation
@@ -54,16 +72,50 @@ app.use(
           title: args.inputEvent.title,
           description: args.inputEvent.description,
           price: +args.inputEvent.price,
-          date: new Date(args.inputEvent.date)
+          date: new Date(args.inputEvent.date),
+          creator: "5e83229a55037f21d280374f"
         });
+        let createdEvent;
         return event
           .save()
           .then(result => {
-            console.log(result);
-            return { ...result._doc };
+            createdEvent = { ...result._doc };
+            return User.findById("5e83229a55037f21d280374f");
+          })
+          .then(user => {
+            if (!user) {
+              throw new Error("User not found");
+            }
+            user.createdEvents.push(event);
+            return user.save();
+          })
+          .then(result => {
+            return createdEvent;
           })
           .catch(err => {
             console.log(err);
+          });
+      },
+      createUser: args => {
+        return User.findOne({ email: args.inputUser.email })
+          .then(user => {
+            if (user) {
+              throw new Error("User exists already");
+            }
+            return bcrypt.hash(args.inputUser.password, 12);
+          })
+          .then(hashedPassword => {
+            const user = new User({
+              email: args.inputUser.email,
+              password: hashedPassword
+            });
+            return user.save();
+          })
+          .then(result => {
+            return { ...result._doc, password: null, _id: result.id };
+          })
+          .catch(err => {
+            throw err;
           });
       }
     },
@@ -76,6 +128,7 @@ mongoose
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-yeq5v.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
+
   .then(() => {
     app.listen(8000);
   })
